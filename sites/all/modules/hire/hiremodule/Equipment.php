1da8eaddd5dece38479f9285e371cb71a91037c7
John Kolovos <yannis.kolovos@gmail.com>
2011-05-12 23:43:09 +1000

    model added

--- sites/all/modules/hire/hiremodule/Equipment.php
+++ sites/all/modules/hire/hiremodule/Equipment.php
@@ -1,88 +1,10 @@
 <?php
-mysql_connect("localhost", "root", "root") or
-die("Could not connect: " . mysql_error());
-mysql_select_db("drupal");
+require_once "model.php";
 
-$debug = true;
-
-/*---------------------------------------*/
-getById ("1");
-getAll();
-/*---------------------------------------*/
-
-
-function getById($id){
-	$q = "SELECT * FROM equipment WHERE id ={$id};";
-
-	$result = mysql_query($q) or die(mysql_error());
-	$stack= getDBTables($result);
-
-	if ($debug=true){ echo "<pre>"; print_r($stack);}
-	return $stack;
-
-}
-
-
-function getAll(){
-	//$q = "SELECT * FROM equipment where status=1;";
-	$q = "SELECT * FROM equipment ;";
-
-	$result = mysql_query($q) or die(mysql_error());
-	$stack= getDBTables($result);
-
-	if ($debug=true){ echo "<pre>"; print_r($stack);}
-	return $stack;
-
-}
-
-
-function getAllByCategory($cat){
-	$q = "SELECT * FROM drupal.equipment group by category ;";
-
-	$result = mysql_query($q) or die(mysql_error());
-	$stack= getDBTables($result);
-
-	if ($debug=true){ echo "<pre>"; print_r($stack);}
-	return $stack;
-
-}
-
-
-function update($eq){
-
-	$q = "";
-}
-
-function delete($id){
-
-	$q = "";
-}
-
-
-
-function add($eq ){
-	$q = "";
-}
-
-
-
-
-
-
-function getDBTables($result)
-{
-	$table_result=array();
-	$r=0;
-	while($row = mysql_fetch_assoc($result)){
-		$arr_row=array();
-		$c=0;
-		while ($c < mysql_num_fields($result)) {
-			$col = mysql_fetch_field($result, $c);
-			$arr_row[$col -> name] = $row[$col -> name];
-			$c++;
-		}
-		$table_result[$r] = $arr_row;
-		$r++;
-	}
-	return $table_result;
-}
+class equipment extends model{
+	function __construct(){
+		parent::__construct('equipment');
+		$this->pageSize == 100000;
+	}}  
+	
+ 
--- sites/all/modules/hire/hiremodule/Hire.php
+++ sites/all/modules/hire/hiremodule/Hire.php
@@ -1,78 +1,8 @@
 <?php
-mysql_connect("localhost", "root", "root") or
-die("Could not connect: " . mysql_error());
-mysql_select_db("drupal");
+require_once "model.php";
 
-$debug = true;
-
-
-
-/*---------------------------------------*/
-getByUserId("1");
-getAll();
-/*---------------------------------------*/
-
-
-
-function getByUserId($id){
-	$q = "SELECT * FROM hire WHERE user_id ={$id};";
-	$result = mysql_query($q) or die(mysql_error());
-	$stack= getDBTables($result);
-
-	if ($debug=true){ echo "<pre>"; print_r($stack);}
-	return $stack;
-}
-
-
-function getAll(){
-	$q = "SELECT * FROM hire ;";
-	$result = mysql_query($q) or die(mysql_error());
-	$stack= getDBTables($result);
-
-	if ($debug=true){print_r($stack);}
-	return $stack;
-}
-
-
-function update($hire = array()){
-
-
-}
-
-function delete($id){
-
-
-}
-
-
-
-function add($hire){
-
-	$q  ="INSERT INTO hire (supplier_id, supplier_name, city)
-VALUES (5005, 'NVIDIA', 'LA');";
-}
-
-
-
-
-function getDBTables($result)
-{
-	$table_result=array();
-	$r=0;
-	while($row = mysql_fetch_assoc($result)){
-		$arr_row=array();
-		$c=0;
-		while ($c < mysql_num_fields($result)) {
-			$col = mysql_fetch_field($result, $c);
-			$arr_row[$col -> name] = $row[$col -> name];
-			$c++;
-		}
-		$table_result[$r] = $arr_row;
-		$r++;
-	}
-	return $table_result;
-}
-
-
-
-?>
+class hire extends model{
+	function __construct(){
+		parent::__construct('hire');
+		$this->pageSize == 100000;
+	}}  
