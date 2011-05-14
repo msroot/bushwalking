0905a9879dd18c57b97df896f26abb551f5154f7
victornava <victornavav@gmail.com>
2011-05-15 00:19:36 +1000

    added select element to hire form

--- sites/all/modules/oosd/oosdmodule/hire.php
+++ sites/all/modules/oosd/oosdmodule/hire.php
@@ -7,8 +7,10 @@ class hire extends model{
 	function __construct(){
 		parent::__construct('hire');
 		$this->pageSize == 100000;
-	}}  
+	}
 	
-	// function getStatuses(){
-	// 	return array("booked, started, finished");
-	// }
+	function getStatuses(){
+		return array("booked", "started", "finished");
+	}
+}  
+	
