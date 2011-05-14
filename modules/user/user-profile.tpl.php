30d799f6521fe0d96a5e1fce3d03463764eccca5
John Kolovos <yannis.kolovos@gmail.com>
2011-05-15 04:06:10 +1000

    user can see the name of equipment

--- modules/user/user-profile.tpl.php
+++ modules/user/user-profile.tpl.php
@@ -22,18 +22,20 @@
 <h3>Membership expires: <?php print_r ($expires_day) ;?> </h3>
 <p>Renew now your Membership with paypall:</p>
 <a href=""><img src="http://www.allprivatelabelcontent.com/paypall.gif"></a>
-
-
-
-
-
 <hr />
+
+
+<br />
+<br />
+
 <h1>Hired Equipments:</h1>
 
-<table border="0" cellpadding="5" cellspacing="5"><tbody>
-<tr><th>equipment_id</th>
-<th>date_start</th>
-<th>date_end</th>
+<table border="0" cellpadding="6" cellspacing="5"><tbody>
+<tr>
+<th>id</th>
+<th>equipment</th>
+<th>date start</th>
+<th>date end</th>
 <th>returned</th>
 <th>status</th>
 <th>pay now</th>
@@ -45,12 +47,16 @@ $q=('SELECT * FROM `hire` WHERE user_id='.$uid.'');
  $hired = pager_query($q, 900, 0, NULL);
 
 while($total = db_fetch_object($hired)) {
-echo '<tr><th>'.$total->equipment_id.'</th>';
+
+  $name = db_result(db_query('SELECT name FROM equipment WHERE id =%d LIMIT 1', $total->equipment_id));
+
+echo '<tr><th>'. $total->equipment_id.' </th>';
+echo '<th>'. $name.' </th>';
 echo '<th>'.$total->date_start.'</th>';
 echo '<th>'.$total->date_end.'</th>';
 echo '<th>'.$total->returned.'</th>';
 echo '<th>'.$total->status.'</th>';
-echo '<th><a href="">pay now</a></th>';
+echo '<th><a href="">pay now</a></th></tr>';
 }
 ?>
 </th></tr></tbody></table>
