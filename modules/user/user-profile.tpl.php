493c467faa868cd2d72ead5add37f34d114d57cb
John Kolovos <yannis.kolovos@gmail.com>
2011-05-15 03:57:11 +1000

    user can now pay and view his staff

--- modules/user/user-profile.tpl.php
+++ modules/user/user-profile.tpl.php
@@ -21,7 +21,40 @@
 <hr />
 <h3>Membership expires: <?php print_r ($expires_day) ;?> </h3>
 <p>Renew now your Membership with paypall:</p>
-<a href=""><img src="http://science-fiction-books.com.au/images/PayPall-logo.gif"></a>
+<a href=""><img src="http://www.allprivatelabelcontent.com/paypall.gif"></a>
+
+
+
+
+
+<hr />
+<h1>Hired Equipments:</h1>
+
+<table border="0" cellpadding="5" cellspacing="5"><tbody>
+<tr><th>equipment_id</th>
+<th>date_start</th>
+<th>date_end</th>
+<th>returned</th>
+<th>status</th>
+<th>pay now</th>
+
+ <?php 
+$uid=$user->uid;
+$q=('SELECT * FROM `hire` WHERE user_id='.$uid.'');
+
+ $hired = pager_query($q, 900, 0, NULL);
+
+while($total = db_fetch_object($hired)) {
+echo '<tr><th>'.$total->equipment_id.'</th>';
+echo '<th>'.$total->date_start.'</th>';
+echo '<th>'.$total->date_end.'</th>';
+echo '<th>'.$total->returned.'</th>';
+echo '<th>'.$total->status.'</th>';
+echo '<th><a href="">pay now</a></th>';
+}
+?>
+</th></tr></tbody></table>
+                  
 
 </div>
 <?php } ?> 
